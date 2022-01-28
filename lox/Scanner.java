7101a3c51f381402d62fc6d2af60e3e47e59a4e7
JInfantesC <jose.infantes@protonmail.com>
2022-01-28 23:22:55 +0100

    Scanner. Identificadores y palabras reservadas

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,7 +1,9 @@
 package lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static lox.TokenType.*;
 
@@ -9,6 +11,29 @@ public class Scanner {
     private final String source;
     private final List<Token> tokens= new ArrayList<>();
     
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords=new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
+
     // To keep track where the scanner is
     private int start=0;
     private int current=0;
@@ -73,11 +98,23 @@ public class Scanner {
                 // Para números, en default comprobamos si es digito para no hacer un case por cada número.
                 if(isDigit(c)){
                     number();
+                }else if(isAlpha(c)){ // Para palabras reservadas e identificadores
+                    identifier();
                 }else{
                     Lox.error(line, "Unexpected character."); // Mezclar varios en un único error sería mejor UX
                 }
                 break;
         }
+    }
+
+    private void identifier(){
+        while (isAlphaNumeric(peek())) advance();
+
+        String text=source.substring(start, current);
+        TokenType type=keywords.get(text);
+        if(type==null) type=IDENTIFIER;
+
+        addToken(type);
     }
 
     private void number(){
@@ -129,6 +166,16 @@ public class Scanner {
         return source.charAt(current+1);
     }
 
+    private boolean isAlpha(char c){
+        return  (c>='a' && c<='z') ||
+                (c>='A' && c<='Z') ||
+                c=='_';
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c)|| isDigit(c);
+    }
+
     private boolean isDigit(char c){
         return c>='0' && c<='9';
     }
@@ -145,6 +192,7 @@ public class Scanner {
         addToken(type, null);
     }
 
+    // Object literal, para que acepte, strings, doubles, o lo que sea.
     private void addToken(TokenType type, Object literal){
         String text= source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
